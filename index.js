aa1865bc018a8a5cd58ebd0241a2272678db8e0f
Mariele <102813740+ladyMariele@users.noreply.github.com>
2024-02-04 17:10:00 +0800

    add webpack.config.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            'style-loader', // Creates `style` nodes from JS strings
-            'css-loader',   // Translates CSS into CommonJS
-            'sass-loader'   // Compiles Sass to CSS
-          ],
-        },
-      ],
-    },
-  };
+import "./assets/CSS/style.scss";
